e4c2230e56cadf8b4e8585341f9433aae9dbe088
liu liu <leoliu@u.northwestern.edu>
2015-07-28 16:55:49 -0500

    init confidence.py

---
+++ confidence.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 16:54:21 2015
+
+@author: lliu5
+"""
+
+def cal_key_conf(result,newKey):
+    '''
+    Args: 
+        Result is the previous result or stored result. 
+        newKey: is the new key passed in.             
+    '''
